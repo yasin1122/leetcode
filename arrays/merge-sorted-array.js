a74c15c42043cbc58b9031df2c26ac71989e7e67
Yasin Cakal <whoisyasin@gmail.com>
2023-08-03 12:34:56 +0300

    first commit

---
+++ arrays/merge-sorted-array.js
@@ -0,0 +1,99 @@
+/*
+You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
+
+Merge nums1 and nums2 into a single array sorted in non-decreasing order.
+
+The final sorted array should not be returned by the function, but instead be stored inside the array nums1. To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged, and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
+
+ 
+
+Example 1:
+
+Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
+Output: [1,2,2,3,5,6]
+Explanation: The arrays we are merging are [1,2,3] and [2,5,6].
+The result of the merge is [1,2,2,3,5,6] with the underlined elements coming from nums1.
+Example 2:
+
+Input: nums1 = [1], m = 1, nums2 = [], n = 0
+Output: [1]
+Explanation: The arrays we are merging are [1] and [].
+The result of the merge is [1].
+Example 3:
+
+Input: nums1 = [0], m = 0, nums2 = [1], n = 1
+Output: [1]
+Explanation: The arrays we are merging are [] and [1].
+The result of the merge is [1].
+Note that because m = 0, there are no elements in nums1. The 0 is only there to ensure the merge result can fit in nums1.
+ 
+
+Constraints:
+
+nums1.length == m + n
+nums2.length == n
+0 <= m, n <= 200
+1 <= m + n <= 200
+-109 <= nums1[i], nums2[j] <= 109
+ 
+
+Follow up: Can you come up with an algorithm that runs in O(m + n) time? 
+*/
+
+/**
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
+var merge = function (nums1, m, nums2, n) {
+  let i = 0
+  let j = 0
+  let temp = []
+
+  while (i < m && j < n) {
+    if (nums1[i] < nums2[j]) {
+      temp.push(nums1[i])
+      i++
+    } else {
+      temp.push(nums2[j])
+      j++
+    }
+  }
+
+  // If nums1 contains remaining elements
+  while (i < m) {
+    temp.push(nums1[i])
+    i++
+  }
+
+  // If nums2 contains remaining elements
+  while (j < n) {
+    temp.push(nums2[j])
+    j++
+  }
+
+  for (let x = 0; x < temp.length; x++) {
+    nums1[x] = temp[x]
+  }
+}
+
+nums1 = [1, 2, 3, 0, 0, 0]
+m = 3
+nums2 = [2, 5, 6]
+n = 3
+merge(nums1, m, nums2, n)
+
+let i = 0
+let j = 0
+
+while (i < 3 || j < 5) {
+  if (i < 3) {
+    i++
+  }
+  if (j < 5) {
+    j++
+  }
+  console.log(i, j)
+}
