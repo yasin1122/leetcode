66db4ac1dca7f0ef9c0aa72b24f51d173a2143e1
Yasin Cakal <whoisyasin@gmail.com>
2024-03-28 03:38:12 +0300

    ab

--- leetcode-75/2-greatest-common-divisor-of-strings.js
+++ leetcode-75/2-greatest-common-divisor-of-strings.js
@@ -33,4 +33,4 @@ function findSmallestRepeatingPattern(str) {
   return str // Return the original string if no repeating pattern is found
 }
 
-console.log(gcdOfStrings('abababab', 'ababab'))
+console.log(gcdOfStrings('ababababab', 'ababab'))
