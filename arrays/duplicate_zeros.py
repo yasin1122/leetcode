3dff0997fe18947a069ba9108a8ec3585cd5b66c
Yasin Cakal <whoisyasin@gmail.com>
2024-06-19 00:35:53 +0300

    added comments

--- arrays/duplicate_zeros.py
+++ arrays/duplicate_zeros.py
@@ -25,9 +25,16 @@ Constraints:
 arr = [1,0,2,3,0,4,5,0]
 
 def duplicateZeros(arr):
+  # convert array of ints into a string
   arr_str = ' '.join(map(str, arr))
+
+  # duplicate 0s and split str into a list
   arr_str = arr_str.replace('0', '0 0').split()
+
+  # map list of str to a list of ints
   arr_str = list(map(int, arr_str))
+
+  # modify arr in place
   for i in range(len(arr)):
     arr[i] = arr_str[i]
 
