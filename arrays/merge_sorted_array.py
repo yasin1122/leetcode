b9e17b6d64a346f601b44ca19fb369310bbe5025
Yasin Cakal <whoisyasin@gmail.com>
2024-06-19 12:24:49 +0300

    optimized version

--- arrays/merge_sorted_array.py
+++ arrays/merge_sorted_array.py
@@ -45,12 +45,22 @@ m, n = 3, 3
 
 
 def merge(nums1, m, nums2, n):
-  if n == 0: 
-    pass
-  else:
-    for i in range(m, m+n):
-      nums1[i] = nums2[i-m]
-  nums1[:] = sorted(nums1)
+  # if n == 0: 
+  #   pass
+  # else:
+  #   for i in range(m, m+n):
+  #     nums1[i] = nums2[i-m]
+  # nums1[:] = sorted(nums1)
+  while m > 0 and n > 0:
+    if nums1[m - 1] > nums2[m - 1]:
+      nums1[m + n - 1] = nums1[m - 1]
+      m -= 1
+    else:
+      nums1[m + n - 1] = nums2[n - 1]
+  
+  while n > 0:
+    nums1[n - 1] = nums2[n - 1]
+    n -= 1
 
 merge(nums1, m, nums2, n)
 print(nums1)
