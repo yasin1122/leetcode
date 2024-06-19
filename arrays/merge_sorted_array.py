ecc7e1f48d236c28f244c8c5dd8aa07efe70670e
Yasin Cakal <whoisyasin@gmail.com>
2024-06-19 12:41:02 +0300

    finalized

--- arrays/merge_sorted_array.py
+++ arrays/merge_sorted_array.py
@@ -51,16 +51,21 @@ def merge(nums1, m, nums2, n):
   #   for i in range(m, m+n):
   #     nums1[i] = nums2[i-m]
   # nums1[:] = sorted(nums1)
+
+  # Start filling the nums1 array from the end
   while m > 0 and n > 0:
-    if nums1[m - 1] > nums2[m - 1]:
-      nums1[m + n - 1] = nums1[m - 1]
-      m -= 1
-    else:
-      nums1[m + n - 1] = nums2[n - 1]
+      if nums1[m - 1] > nums2[n - 1]:
+          nums1[m + n - 1] = nums1[m - 1]
+          m -= 1
+      else:
+          nums1[m + n - 1] = nums2[n - 1]
+          n -= 1
   
+  # If there are remaining elements in nums2, copy them
   while n > 0:
-    nums1[n - 1] = nums2[n - 1]
-    n -= 1
+      nums1[n - 1] = nums2[n - 1]
+      n -= 1
+
 
 merge(nums1, m, nums2, n)
 print(nums1)
