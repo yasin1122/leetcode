92881d7b45c6ed554a2bb20cd1fb23bac51f7526
yasin1122 <whoisyasin@gmail.com>
2024-07-11 15:48:45 +0300

    ll append

---
+++ s5_LL_Exercises/e5_Append.py
@@ -0,0 +1,17 @@
+def append(self, value):
+    # Create a new node with the given value
+    new_node = Node(value)
+ 
+    # Check to see if the linked list is empty
+    if self.head is None:
+        # Point both head and tail at the new node
+        self.head = new_node
+        self.tail = new_node
+    else:
+        # Point the next pointer of the last node at the new node
+        self.tail.next = new_node
+        # Point tail at the new node
+        self.tail = new_node
+ 
+    # Increment the length of the linked list
+    self.length += 1
